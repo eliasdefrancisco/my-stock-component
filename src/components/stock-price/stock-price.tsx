d433e3ed648a56824a999eb1d39814af7df3830c
Elias de Francisco <eliasdefrancisco@gmail.com>
2019-08-28 18:41:51 +0200

    Fetching a value

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from "@stencil/core";
+import { Component, h, State } from "@stencil/core";
 
 @Component({
     tag: 'my-stock-price',
@@ -6,21 +6,32 @@ import { Component, h } from "@stencil/core";
     shadow: true
 })
 export class StockPrice {
+    @State() fetchedPrice: number
+
     apikey = 'Q6V3K6756DWWTM11' // Temporary api key from Alpha Vantage
 
     onFetchStockPrice(event: Event) {
         event.preventDefault()
-        console.log('Submitted!')
+        fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo')
+        .then(res => {
+            return res.json()
+        })
+        .then(parsedRes => {
+            this.fetchedPrice = +parsedRes['Global Quote']['05. price']
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 
     render() {
         return [
-            <form onSubmit={ this.onFetchStockPrice }>
+            <form onSubmit={ this.onFetchStockPrice.bind(this) }>
                 <input id="stock-symbol" />
                 <button type="submit">Fetch</button>
             </form>,
             <div>
-                <p>Price: { 0 }</p>
+                <p>Price: { this.fetchedPrice }</p>
             </div>
         ]
     }
