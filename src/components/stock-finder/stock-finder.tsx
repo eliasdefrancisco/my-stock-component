8480503f5620b5dc34c6bc8a6cebbe818b880e3a
Elias de Francisco <eliasdefrancisco@gmail.com>
2019-09-03 17:39:03 +0200

    StockFinder compnent functionality

--- src/components/stock-finder/stock-finder.tsx
+++ src/components/stock-finder/stock-finder.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from "@stencil/core";
+import { Component, h, State } from "@stencil/core";
 import { AV_API_KEY} from "../../global/global"
 
 @Component({
@@ -9,13 +9,17 @@ import { AV_API_KEY} from "../../global/global"
 export class StockFinder {
     stockNameInput: HTMLInputElement
 
+    @State() searchResults: { symbol: string, name: string }[] = []
+
     onFindStocks(event: Event) {
         event.preventDefault()
         const stockName = this.stockNameInput.value
         fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockName}&apikey=${AV_API_KEY}`)
         .then(res => res.json())
         .then(parsedRes => {
-            console.log(parsedRes)
+            this.searchResults = parsedRes['bestMatches'].map(match => {
+                return { name: match['2. name'], symbol: match['1. symbol'] }
+            })
         })
         .catch(err => console.log(err))
     }
@@ -28,7 +32,18 @@ export class StockFinder {
                 ref={ el => this.stockNameInput = el }
                 />
                 <button type="submit">Find</button>
-            </form>
+            </form>,
+            <ul>
+                { 
+                    this.searchResults.map(result => (
+                        <li>
+                            <strong>{ result.symbol }</strong>
+                             - { result.name }
+                        </li>
+                    )) 
+                
+                }
+            </ul>
         ]
     }
 }
